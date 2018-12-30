71df06d6f1b4c2350ebd770f7272473870b634b8
Sumitra Badrinathan <45888950+SumitraBadrinathan@users.noreply.github.com>
2018-12-30 12:46:10 -0500

    Update 2-DataManipulation.R

--- 2-DataManipulation.R
+++ 2-DataManipulation.R
@@ -1,17 +1,24 @@
-### Lab 2: Data Manipulation ###
+#  #######################################################################
+#       File-Name:      2-DataManipulation.R
+#       Version:        R 3.4.3
+#       Date:           Sep 04, 2018
+#       Author:         Sumitra Badrinathan <sumitra@sas.upenn.edu>
+#       Purpose:        Reading in and manipulating data on Amnesty's
+#                       assessment of state terror in 1994
+#       Machine:        macOS  10.14
+#  #######################################################################
 
-# See your current working directory
-getwd()
+set.seed(1221)
+rm(list=ls()) # remove objects from R workspace
 
-# Set your own working directory (where the relevant files are)
+# set working directory
 setwd("~/Dropbox/PSCI338") #macs
 #setwd("C:/Users/name/Dropbox/PSCI338") #windows
 
-# Clears the workspace
-rm(list = ls())
+# Download data file pts1994.csv from this repository and save it in the Data/Raw folder
+# Read in the data : use read.csv for csv files or read.table for txt files
 
-# Read your data file (use read.csv for csv files or read.table for txt files)
-# Reads in .csv with data on assessment of state terror in 1994
+# reads in .csv with data on assessment of state terror in 1994
 democracy94 <- read.csv("Data/Raw/pts1994.csv")
 #democracy94 <- read.table("Data/Raw/pts1994.csv", header=TRUE, na="NA", sep=",")
 
@@ -19,70 +26,72 @@ democracy94 <- read.csv("Data/Raw/pts1994.csv")
 class(democracy94) #this is a dataframe, like a spreadsheet
 
 # see a snippet of the full data
-head(democracy94)
-head(democracy94,15)
-names(democracy94)
+head(democracy94) # first few rows
 
-# See the data for one country
-democracy94[1,]
-democracy94[1:10] #will this work?
+# data contains 6 variables and 179 rows, each row a country. Variables are country name,
+# COW country code, world bank country code, 
+# and amnesty & state departement's assessment of terror in 1994 on a scale of 1 to 5 (higher=more terror)
 
-# See the data for one variable, say COW (there are two ways to do this)
+head(democracy94,15) # specify first 15 rows
+names(democracy94) # see names of variables in datsaet
+
+# see the data for one country
+democracy94[1,] # with a dataframe, have to specify rows AND columns to be displayed
+democracy94[1:10] # will this work?
+
+# see the data for one variable, say Amnesty score (there are two ways to do this)
 democracy94[,5] # notice missing data!
 democracy94$Amnesty.1994
 
-#summary of a variable
+# summary of a variable
 summary(democracy94$Amnesty.1994)
 
-#tabulate the amnesty variable, ignoring missings
+# tabulate the amnesty variable, ignoring missings
 table(democracy94$Amnesty.1994)
 
-#tabulate amnesty var, including missings
+# tabulate amnesty var, including missings
 table(democracy94$Amnesty.1994, useNA = "ifany")
 
-#create a table w proportions 
+# create a table w proportions 
 prop.table(table(democracy94$Amnesty.1994))
 
-#basic numerical stats
+# basic numerical stats
 max(democracy94$Amnesty.1994) #notice NA! how to remove?
 mean(democracy94$Amnesty.1994, na.rm = T)
 var()
 sd()
 
-#subsetting! 
+# subsetting! 
 head(democracy94)
 #subset to only certain variables
 reduced <- subset(democracy94, select=c(Country, Amnesty.1994))
 head(reduced)
-dim(reduced)
+dim(reduced) # see dimensions of subsetted data
 
-#subset to values: display cases where amnesty score is <=2
+# subset to certain rows: display cases where amnesty score is <=2
 lowterror <- subset(democracy94, democracy94$Amnesty.1994 <= 2)
 head(lowterror,10)
 table(lowterror$Amnesty.1994)
 dim(lowterror)
 
-#logical operators in R: evaluated to TRUE or FALSE
-letters <- "abcdef"
-letters=="abc"
-
+# logical operators in R: evaluated to TRUE or FALSE
 is.data.frame(democracy94)
 is.matrix(democracy94)
-class(letters)
 
-#subset with missings
+# subset with missings
 lowterror2 <- subset(democracy94, democracy94$Amnesty.1994 <= 2 | is.na(democracy94$Amnesty.1994))
 dim(lowterror2)
 
-# Display cases where the two sources differ by more than one unit
+# display cases where the two sources differ by more than one unit
 disagree <- subset(democracy94, abs(Amnesty.1994 - StateDept.1994) >= 2)
 dim(disagree)
 disagree
 
-#create a new variable to display which countries have an amnesty score of 5 AND state dep score of 5
+# create a new variable to display which countries have an amnesty score of 5 AND state dep score of 5
 democracy94$highterror <- as.numeric(democracy94$Amnesty.1994==5 & democracy94$StateDept.1994==5)
 head(democracy94,10) #we created a new variable!
 table(democracy94$highterror)
+# which are those countries?
 democracy94$Country[democracy94$highterror==1]
 
 # exercise in spurious correlations
