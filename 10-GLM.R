a9b9472246ef5ce21be023649e0ee5ef5d2c8078
Sumitra Badrinathan <45888950+SumitraBadrinathan@users.noreply.github.com>
2019-01-04 16:22:45 -0500

    Update and rename Lab10-GLM.R to 10-GLM.R

--- 10-GLM.R
+++ 10-GLM.R
@@ -1,25 +1,41 @@
-##### GLM and Predicted Probabilities ####
+# #######################################################################
+#       File-Name:      10-GLM.R
+#       Version:        R 3.4.3
+#       Date:           Dec 10, 2018
+#       Author:         Sumitra Badrinathan <sumitra@sas.upenn.edu>
+#       Purpose:        Logit & probit regressions to model admit success probability
+#                       + predicted probability plots + marginal effects
+#       Machine:        macOS  10.14
+# #######################################################################
 
-rm(list = ls())
+set.seed(1221)
+rm(list=ls()) # remove objects from R workspace
 
-setwd("~/Dropbox/PSCI338")
+# set working directory
+setwd("~/Dropbox/PSCI338") #macs
+#setwd("C:/Users/name/Dropbox/PSCI338") #windows
 
-# Admission example
+# load and install required packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(readstata13, margins, effects)
 
-library(readstata13)
+# Download data file Binary.dta from this repository and save it in the Data/Raw folder
+# File contains data on whether or not students were admitted to grad school. 
+# We will model admit success as a function of GRE, GPA, and rank of undergrad school
+
+# load in data
 binarydata <- read.dta13("Data/Raw/Binary.dta")
-head(binarydata)
-
 stargazer(binarydata, type = "text")
 
-# run OLS
+# run OLS regressing admit on GRE + GPA + rank of school
 binarydata$rank.f <- factor(binarydata$rank)
 reg1 <- lm(admit ~ gre + gpa + rank.f, data = binarydata)
 summary(reg1)
+
 # save predicted values in separate variable
 binarydata$predictedvalues <- predict(reg1)  
 
-# there are values below 0
+# are there values below 0?
 summary(binarydata$predictedvalues)
 belowzero <- subset(binarydata, predictedvalues < 0)
 print(belowzero)
@@ -27,28 +43,26 @@ print(belowzero)
 # use a logit model on the same data
 output <- glm(admit ~ gre + gpa + rank.f, family=binomial(logit), data = binarydata)
 summary(output)
-
 # interpret constant: -3.98 is the log odds of getting into a school when everything else is 0
 
 # predicted prob of Y for the first row
-# Calculates predicted probability of acceptenace for someone with a GRE of 380, 
-#GPA of 3.61, and from undergraduate school in the third quartile
+# calculates predicted probability of acceptenace for someone with a GRE of 380, 
+# GPA of 3.61, and from undergraduate school in the third quartile
 binarydata[1,]
 exp(-3.989979 + 0.002264*380+0.804038*3.61+-0.675443*0+-1.340204*1+-1.551464*0) / 
         (1 + exp(-3.989979 + 0.002264*380+0.804038*3.61+-0.675443*0+-1.340204*1+-1.551464*0))
 
-
 # plot predicted probabilities for all ranks
 
-# Gives the values that you want to set the variable to
+# give the values that you want to set the variable to
 inputs <- cbind(1, 380, 3.61, 1:4)
-# Specifies the names of the variables that you want to apply these values to
+# specify the names of the variables that you want to apply these values to
 colnames(inputs) <- c("constant", "gre", "gpa", "rank.f")
-# Turns into a data frame
+# turn into a data frame
 inputs <- as.data.frame(inputs)
-# Because rank.f is a factor, need to turn into a factor within the dataframe
+# because rank.f is a factor, need to turn into a factor within the dataframe
 inputs$rank.f <- as.factor(inputs$rank.f)
-# Gets the predicted probability of voting for every combination of covariates included in inputs and puts into vector forecast
+# gets the predicted probability of voting for every combination of covariates included in inputs and puts into vector forecast
 forecast <- predict(output, newdata = inputs, type = "response")
 print(forecast)
 
@@ -61,30 +75,29 @@ axis(side=1, at=c(1, 2, 3, 4), labels = TRUE)
 title(main="Rank of Undergraduate School and Probability of Grad School Acceptance", 
       sub="Applicant with 380 GRE and 3.61 GPA")
 
-# plotting predicted probabilities
-install.packages("margins")
-library(margins)
+# plotting predicted probabilities (from the "margins" package))
 
 # prediced prob of being admitted given rank
 cplot(output, "rank.f", what = "prediction", 
-      main = "Predicted Admit prob, Given Rank")
+      main = "Predicted Admit Probability, Given Rank")
 
 # prediced prob of being admitted given gpa
-cplot(output, "gpa", what = "prediction", main = "Predicted Admit prob, Given GPA",
+cplot(output, "gpa", what = "prediction", main = "Predicted Admit Probability, Given GPA",
       col = "navyblue", se.type = "shade", se.fill = "lightblue")
-
-# if you want all effects
-library(effects)
-plot(allEffects(output))
 
 # marginal effect of gpa
 m <- margins(output) 
 summary(m)
 
 # marginal effect
-#predicted increment of the response variable 
-#associated with a unit increase in one of the covariates keeping the others constant.
+# predicted increment of the response variable 
+# associated with a unit increase in one of the covariates keeping the others constant.
 cplot(output, "gpa", what = "effect", main = "Marginal Effect of GPA")
+
+
+# if you want a predicted probability plot for each predictor variable
+# use the plot(allEffects()) command from the "effects" package
+plot(allEffects(output))
 
 # probit example
 output2 <- glm(admit ~ gre + gpa + rank.f, family=binomial(probit), data = binarydata)
